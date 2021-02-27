4728f52285c4165d3d481b8f5e94eb7b3a7e0062
bear <bear@ideamake.cn>
2021-02-27 16:51:42 +0800

    修改xml

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,9 +1,9 @@
 <template>
   <v-footer app padless absolute style="background:rgba(0,0,0,0)">
     <v-col class="text-center footer" cols="12">
-      <div>Copyright &copy; 2020-{{ new Date().getFullYear() }}</div>
+      <div>Copyright &copy; 2021-{{ new Date().getFullYear() }}</div>
       <a href="http://www.beian.miit.gov.cn" target="_blank">
-        渝ICP备2020012273号</a
+        	粤ICP备2021015885号-1</a
       >
     </v-col>
   </v-footer>
