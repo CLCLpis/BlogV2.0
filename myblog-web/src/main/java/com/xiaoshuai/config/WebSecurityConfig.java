9c9c0314122360954e7b2842b6fa38e8302c996a
bear <bear@ideamake.cn>
2021-02-27 22:14:03 +0800

    xiugai

--- myblog-web/src/main/java/com/xiaoshuai/config/WebSecurityConfig.java
+++ myblog-web/src/main/java/com/xiaoshuai/config/WebSecurityConfig.java
@@ -105,8 +105,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         // 同源配置，*表示任何请求都视为同源，若需指定ip和端口可以改为如“localhost：8080”，多个以“，”分隔；
+        corsConfiguration.addAllowedOrigin("http://localhost:80");
         corsConfiguration.addAllowedOrigin("http://localhost:8080");
         corsConfiguration.addAllowedOrigin("http://106.55.61.15:8080");
+        corsConfiguration.addAllowedOrigin("http://106.55.61.15:80");
         // header，允许哪些header
         corsConfiguration.addAllowedHeader("*");
         // 允许的请求方法，PSOT、GET等
