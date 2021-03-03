765d71a19c7077840f60531d781dc614be741a17
bear <bear@ideamake.cn>
2021-03-03 21:21:17 +0800

    xiugai

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,7 +2,7 @@ import Axios from "axios";
 
 const instance = Axios.create({
   baseURL: 'https://web.bear0901.cn:32443/blog',
-  timeout: 3000,
+  timeout: 10000,
   withCredentials: true, // default
 });
 
